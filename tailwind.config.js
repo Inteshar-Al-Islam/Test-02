aaeb255031add10b565a536e442379fc9783ce73
Inteshar-Al-Islam <deadperson2047@gmail.com>
2024-09-14 20:24:08 +0600

    initiallization

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          montserrat: ['Montserrat', 'sans-serif'],
+          manrope: ['Manrope', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
